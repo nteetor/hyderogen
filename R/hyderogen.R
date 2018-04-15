c97be4d288ce102168b347046448f7021c6b092d
nteetor <nathanteetor@gmail.com>
2018-04-15 16:51:50 -0400

    cleaned up folder structure, better docs

--- R/hyderogen.R
+++ R/hyderogen.R
@@ -1,3 +1,24 @@
+#' Build jekyll sites
+#'
+#' @description
+#'
+#' hyderogen provides a toolset for quickly building Jekyll projects from your
+#' package's roxygen. These tools focus on creating detailed front matter and
+#' plain templates. You can then further customize these templates. When you
+#' update your roxygen you can rebuild your Jekyll front matter, while leaving
+#' the templates untouched.
+#'
+#' This package is a work in progress. If you try it out please do not be shy
+#' about reporting bugs or less-than-ideal results. All feedback is welcome at
+#' [nteetor/hyderogen/issues](https://github.com/nteetor/hyderogen).
+#'
+#' @seealso
+#'
+#' For more on Jekyll check out \url{https://jekyllrb.com/docs/home/}.
+#'
+#' For more on using Jekyll and Github pages check out,
+#' \url{https://jekyllrb.com/docs/github-pages/}.
+#'
 #' @import purrr fs yaml
 #' @name hyderogen
 "_PACKAGE"
--- R/jekyll.R
+++ R/jekyll.R
@@ -1,3 +1,22 @@
+#' Build a jekyll site
+#'
+#' The `jekyll` function builds a jekyll site from your
+#' package's roxygen.
+#'
+#' @param pkg A character string specifying a file path to an R package,
+#'   defaults to `"."`, the current directory.
+#'
+#' @param dir A folder path, relative to `pkg`, where the jekyll site will be
+#'   built, defaults to `"docs"`.
+#'
+#' @export
+#' @examples
+#' \dontrun{
+#'
+#' jekyll("~/git/mypkg")
+#'
+#' }
+#'
 jekyll <- function(pkg = ".", dir = "docs") {
   r_dir <- path(pkg, "R")
 
@@ -8,16 +27,12 @@ jekyll <- function(pkg = ".", dir = "docs") {
     )
   }
 
-  if (dir != stdout() && !dir_exists(dir)) {
-    dir_create(dir)
-  }
-
   proj <- parse_files(r_dir)
 
   if (dir == stdout()) {
     cat(format_project(dir), sep = "\n")
   } else {
-    write_project(proj, dir = dir)
+    write_project(proj, dir = path(pkg, dir))
   }
 
   invisible()
